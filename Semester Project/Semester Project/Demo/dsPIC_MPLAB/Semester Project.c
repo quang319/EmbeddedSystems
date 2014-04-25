74917945cadeffa9ca88e70dbb9b687639d755a7
Quang Nguyen <quang319@gmail.com>
2014-04-25 15:35:06 -0400

    Pre-change in architecture

--- Semester Project/Semester Project/Demo/dsPIC_MPLAB/Semester Project.c
+++ Semester Project/Semester Project/Demo/dsPIC_MPLAB/Semester Project.c
@@ -21,6 +21,7 @@
  *
  *******************************************************************************/
 #include <p33FJ256GP710A.h>
+#include "ADC.h"
 
 /* Standard includes. */
 #include <stdio.h>
@@ -31,6 +32,7 @@
 #include "queue.h"
 #include "croutine.h"
 #include <portmacro.h>
+#include <semphr.h>
 
 /* Demo application includes. */
 #include "BlockQ.h"
@@ -41,6 +43,9 @@
 #include "partest.h"
 #include "lcd.h"
 #include "timertest.h"
+
+/* User's includes  */
+#include "ADC.h"
 
 /*******************************************************************************
  *
@@ -58,26 +63,55 @@ _FWDT(FWDTEN_OFF);
  *******************************************************************************/
 #define TRUE 1
 #define FALSE 0
+
+/*******************************************************************************
+ *
+ *                      Type definition
+ *
+ *******************************************************************************/
+typedef struct
+{
+    char Line1[17];
+    char Line2[17];
+} xLCD;
 /*******************************************************************************
  *
  *                      Global Variable Declarations
  *
  *******************************************************************************/
 xQueueHandle LCDDisplayinfo;
-char RTCFlag; //This global variable can be used to signal that the button to display the RTC
+char RTCFlag = 1; //This global variable can be used to signal that the button to display the RTC
 //has been pressed.  Ideally we could have done this with a semiphore.
+
+
+/*******************************************************************************
+ *
+ *                      Semaphore Declarations
+ *
+ *******************************************************************************/
+xSemaphoreHandle DmaSemaphore;
+/*******************************************************************************
+ *
+ *                      Task Declarations
+ *
+ *******************************************************************************/
+void buttonPushCounter( void *pvParameters);
+void rtcTimer(void *pvParameters);
+void writeLcd (void *pvParameters);
+void prvSetupHardware( void );
+void vApplicationIdleHook(void);
+
+void dmaHandler (void *pvParameters);
+
 /*******************************************************************************
  *
  *                      Function Prototype
  *
  *******************************************************************************/
 void initialize();
-static void prvSetupHardware( void );
+
 char IntToChar(unsigned int Digit);
-static void ButtonPushCounter( void *pvParameters);
-void RTCTimer(void *pvParameters);
-void WriteLCD (void *pvParameters);
-void vApplicationIdleHook(void);
+
 
 void displayCounter(int Count, char *LCD);
 void digitCheck(int *FirstDigit, int *SecondDigit);
@@ -94,7 +128,7 @@ int main() {
 
     /*************** Main Loop ***********************************/
     /* Will only reach here if there is insufficient heap available to start
-	the scheduler. */
+    the scheduler. */
     while (1) {
 
     } // End of the infinite While loop
@@ -110,19 +144,36 @@ int main() {
 void initialize() {
     /* Configure any hardware required for this demo. */
     PLLFBD = 0x001E; //external osc is 8MHz using defaults Fosc=8MHZ /8 (0x1E+2)=32 MHz
+
     //RTOS setup of hardware
     prvSetupHardware();
+    /*****************************************************
+    *               Setting up DMA and Semaphore
+    *****************************************************/
+    // // Setting up ADC for DMA Interrupt
+    // ADC_Init();
+    // // Create semaphore for DMA interrupt
+    // vSemaphoreCreateBinary(DmaSemaphore);
+    // // Create Semaphore handle
+    // xTaskCreate(dmaHandler, "Task for DMA", 500, NULL, 3, NULL );
+    
+    /*****************************************************
+    *               Initialize LCD
+    *****************************************************/
     Init_LCD();//Initialize the LCD this must be done first before the LCD is to be used.
     while (check_busy())
     {
     }
     home_clr(); // start display at top line left most character
-    /* Create Tasks*/
-    xTaskCreate(ButtonPushCounter,"Task Read Swtchs", 200, NULL, 2, NULL);
-    xTaskCreate(RTCTimer, "Task RTC", 200, NULL, 2, NULL);
-    xTaskCreate(WriteLCD, "Write LCD",200, NULL, 1, NULL); //notice that this task has lower priority.
+
+    /*****************************************************
+    *               Create Tasks and Queue
+    *****************************************************/
+    xTaskCreate(buttonPushCounter,"Task Read Swtchs", 200, NULL, 2, NULL);
+    xTaskCreate(rtcTimer, "Task RTC", 200, NULL, 2, NULL);
+    xTaskCreate(writeLcd, "Write LCD",200, NULL, 1, NULL); //notice that this task has lower priority.
     /* create the queue for the string to be displayed.  Length = 2 each chunk holds 16 char string*/
-    LCDDisplayinfo = xQueueCreate(2,16);
+    LCDDisplayinfo = xQueueCreate(1,sizeof (xLCD));
 
     /* start the scheduler. */
     vTaskStartScheduler();
@@ -133,26 +184,54 @@ void initialize() {
  *                      Interrupt Service Routine
  *
  *******************************************************************************/
-
+// void __attribute__((interrupt)) _DMA0Interrupt(void)
+// {
+//     static portBASE_TYPE HigherPriority;
+//     xSemaphoreGiveFromISR (DmaSemaphore, &HigherPriority);
+//     _DMA0IF = 0;
+// }
 /*******************************************************************************
  *
  *                      ROS tasks
  *
  *******************************************************************************/
 //This is required by the RTOS, DO NOT MOTIFY
-static void prvSetupHardware( void )
+void prvSetupHardware( void )
 {
-	vParTestInitialise();
+    vParTestInitialise();
 }
 
+// void dmaHandler (void *pvParameters)
+// {
+//     int *DMAptr;
+//     unsigned int VSignal[120], ISignal[120];
+//     int Vrms = 0;
+//     int Irms = 0;
+//     while(1)
+//     {
+//         xSemaphoreTake(DmaSemaphore, portMAX_DELAY);
+
+//         int i;
+//     DMAptr = BeginDMA;
+//     for (i=0; i<120; i++)
+//         {
+//             VSignal[i] = *DMAptr;
+//             DMAptr++;
+//             ISignal[i] = *DMAptr;
+//             DMAptr++;
+//         }
+//         Vrms = rmsCalc(VSignal);
+//     }
+// }
 
 /*-----------------------------------------------------------*/
-static void ButtonPushCounter( void *pvParameters)
+void buttonPushCounter( void *pvParameters)
 {
     /* This task will read the pushbuttons, since most pushes of the button will take more than
     10 ms it needs to only run every 10 ms or l00ms if in the middle of a button push read.
     The API for a delay is: vTaskDelay(10); to delay for 10ms before being active again */
 
+    /*
     char LCDDisplay[17] = "Count = 00000   ";
 
     int Rd6ChangeStateFlag = 0;
@@ -161,12 +240,13 @@ static void ButtonPushCounter( void *pvParameters)
     int BlockingFlag = 0;           // 0 = 10 ms
                                     // 1 = 100 ms
     xQueueSendToBack (LCDDisplayinfo, LCDDisplay, 0);
-    xQueueSendToBack (LCDDisplayinfo, LCDDisplay, 0);
+    */
 
     /* start endless loop*/
     for( ;; )
     {
 
+        /*
         // if RD6(S3) is pressed
         // Display count on LCD
         if ( (_RD6 == 0) && (Rd6ChangeStateFlag == 0))
@@ -214,27 +294,31 @@ static void ButtonPushCounter( void *pvParameters)
         else
             vTaskDelay(100);
 //            vTaskDelay( 1000 / portTICK_RATE_MS);
+        */
+        vTaskDelay( 1000 / portTICK_RATE_MS);
     } // end of for loop
 }
 
-void WriteLCD(void *pvParameters)
+void writeLcd(void *pvParameters)
 {
     // This is lower priority task it will write charaters to the LCD screen in response to the button
     //pushes an example API of reading the queue is given
 
     int LCDCounter = 0;
+    char SecondLineFlag = 0;
 
     portBASE_TYPE NewString;
-    char LCDDisplayString[16];
+    xLCD LCDDisplayString;
     for ( ; ; )
     {
-        NewString = xQueueReceive( LCDDisplayinfo,LCDDisplayString,10);
+        NewString = xQueueReceive( LCDDisplayinfo,&LCDDisplayString,10);
 
         // If we recieve a new message.
         // Need to bring the display back to the beginning and start displayin the new data
         if (NewString == pdPASS)
         {// get new string and start displaying
             LCDCounter = 0;
+            SecondLineFlag = 0;
             home_it();
             vTaskDelay( 10 / portTICK_RATE_MS);
         }
@@ -247,11 +331,24 @@ void WriteLCD(void *pvParameters)
             //Check if busy
             if (check_busy() == 0)
             {
-                // if LCDCounter is not at the end of the string
-                if (LCDCounter < 17)
+                // Print line 1
+                if (LCDCounter < 16)
                 {
                     // Printer Character on LCD
-                    lcd_data( LCDDisplayString[LCDCounter] );
+                    lcd_data( LCDDisplayString.Line1[LCDCounter] );
+                    LCDCounter++;
+                }
+                // move to next line
+                else if ( (LCDCounter >= 16) && (SecondLineFlag == 0) )
+                {
+                    line_2();
+                    SecondLineFlag = 1;
+                }
+                // Print line 2
+                else if ((LCDCounter >= 16) && (SecondLineFlag == 1) )
+                {
+                    // Printer Character on LCD
+                    lcd_data( LCDDisplayString.Line2[LCDCounter - 16] );
                     LCDCounter++;
                 }
                 vTaskDelay( 10 / portTICK_RATE_MS);
@@ -260,7 +357,7 @@ void WriteLCD(void *pvParameters)
     }//end of for loop
 }
 
-void RTCTimer(void *pvParameters)
+void rtcTimer(void *pvParameters)
 { //This is where you place your code to keep track of the RTC and the display that it will show.
   //Initialize and declare
     /* In order to get true timing you should use the periodic delay function to set this up
@@ -270,58 +367,13 @@ void RTCTimer(void *pvParameters)
 
     portTickType xLastWakeTime;
     xLastWakeTime = xTaskGetTickCount();
-    char LCDDisplay[16];
-
-    int Z = 0, S = 0, M = 0, H = 0;
+    xLCD LCDDisplay = {.Line1 = "Hello World!    ", .Line2 = "I am awesome!   "};
     for( ;; )
     {
-        // Z counts in half a second increment.
-        // If Z overflowed
-        Z++;
-        if ( Z == 2)
-        {
-            Z = 0;
-            S++;
-        }
-        // If S overflowed
-        if ( S > 60)
-        {
-            S = 0;
-            M++;
-        }
-        // if M overflowed
-        if ( M > 60)
-        {
-            M = 0;
-            H++;
-        }
-        // if H overflowed
-        if ( H > 24)
-        {
-            H = 0;
-        }
-
-        // Load the LCDDispaly array with the time if the RTCFlag is HIGH
-        if (RTCFlag == 1)
-        {
-            char Temp[5] = "RTC ";
-            int Counter;
-            for (Counter =0 ; Counter <= 3; Counter++)
-            {
-                LCDDisplay[Counter] = Temp[Counter];
-            }
-            BinaryToDecimal( H, &LCDDisplay[4], &LCDDisplay[5]);
-            LCDDisplay[6] = ':';
-            BinaryToDecimal( M, &LCDDisplay[7], &LCDDisplay[8]);
-            LCDDisplay[9] = ':';
-            BinaryToDecimal( S, &LCDDisplay[10], &LCDDisplay[11]);
-            LCDDisplay[12] = '.';
-            if ( Z == 1)
-                LCDDisplay[13] = '5';
-            else LCDDisplay[13] = '0';
-            xQueueSendToBack(LCDDisplayinfo, LCDDisplay, 0);
-        }
-        vTaskDelayUntil(&xLastWakeTime,500); // wait 0.5 seconds
+        
+        xQueueSendToBack(LCDDisplayinfo, &LCDDisplay, 0);
+        // The LCD needs more than .5 second to print the 2 lines
+        vTaskDelayUntil(&xLastWakeTime,700); // wait 0.5 seconds
     }
 }
 
@@ -339,104 +391,3 @@ void vApplicationIdleHook( void)
  *                      Other Functions
  *
  *******************************************************************************/
-/***************************************************
- *
- * Name:            displayCounter
- * Parameters:
- *          int Count
- * Return:  void
- * Pins used:        None
- * Global Var used:
- *          char            LCDDisplay[17];
- * Function:
- *          Similiar to the binaryToDecimal Function.
- *          This function will take the value of count and convert
- *          it to decimal.
- *
- ***************************************************/
-void displayCounter(int Count, char *LCD)
-{
-    //Goal: to display "Count = XXXXX   "
-    char TempChar[9] = "Count = ";
-    int Counter;
-    for (Counter =0 ; Counter < 8; Counter++)
-    {
-        LCD[Counter] = TempChar[Counter];
-    }
-
-
-    int Ten = 0, Hundred = 0, Thousand = 0, TenThousand = 0;
-    if (Count > 9)
-    {
-        while(Count > 9)
-        {
-            Count -= 10;
-            Ten++;
-            digitCheck(&Ten, &Hundred);
-            digitCheck(&Hundred, &Thousand);
-            digitCheck(&Thousand, &TenThousand);
-        }
-    }
-
-    LCD[12] = Count + 48;
-    LCD[11] = Ten + 48;
-    LCD[10] = Hundred + 48;
-    LCD[9] = Thousand + 48;
-    LCD[8] = TenThousand + 48;
-    LCD[13] = ' ';
-}
-
-/***************************************************
- *
- * Name:            digitCheck
- * Parameters:
- *          int *FirstDigit
- *          int *SecondDigit
- * Return:
- *          void
- * Pins used:        None
- * Global Var used:
- * Function:
- *          This function will look at the pointer for the first digit
- *          and increment the second digit if it is greater than 9.
- *
- ***************************************************/
-void digitCheck(int *FirstDigit, int *SecondDigit)
-{
-    if ((*FirstDigit) > 9)
-    {
-        *FirstDigit -= 10;
-        (*SecondDigit)++;
-    }
-}
-
-
-/***************************************************
- *
- * Name:            BinaryToDecimal
- * Parameters:
- *          int value : The value of time
- *          char *TenthPlace : Pointer to the variable for the tenth place
- *          char *OnePlace : Pointer to the variable for the one place
- * Return:
- *          void
- * Pins used:        None
- * Function:
- *          Function will convert will convert a binary value to decimal value
- *
- ***************************************************/
-void BinaryToDecimal ( int Value, char *TenthPlace, char *OnePlace)
-{
-    int Temp = 0;
-    if (Value > 9)
-    {
-        while(Value > 9)
-        {
-            Value -= 10;
-            Temp++;
-        }
-    }
-    // Assign value to the variable and convert it to ASCII
-    *TenthPlace = 48 + Temp;
-    *OnePlace = 48 + Value;
-}
